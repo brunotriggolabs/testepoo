50abfad61ae4e488b5e572d265e971d7d12fc2da
brunomedeirosrafael@gmail.com <brunomedeirosrafael@gmail.com@a6c85282-3ebc-2b4b-b03f-bf2401319e31>
2011-09-28 20:18:05 +0000

    Criadas as classes sobre nossa responsabilidade (Iago e Bruno) e seus relacionamentos.

---
+++  poo-tp1/TP1/src/modelo/Pessoa.java
@@ -0,0 +1,22 @@
+package modelo;
+
+public abstract class Pessoa {
+	protected String nome;
+	protected String cpf;
+	protected String endereco;
+	protected String telefone;
+	
+	public Pessoa(String nome, String cpf) {
+		this.cpf = cpf;
+		this.nome = nome;
+	}
+
+	protected void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	protected void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+}
