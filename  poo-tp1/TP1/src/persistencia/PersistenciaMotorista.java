74471c690c6518ff1ab85b75a5d5adb8147b0004
brunomedeirosrafael@gmail.com <brunomedeirosrafael@gmail.com@a6c85282-3ebc-2b4b-b03f-bf2401319e31>
2011-09-29 16:40:43 +0000

    add uso de arquivo(comentamos alguns codigos de vocês, para o programa compilar).

---
+++  poo-tp1/TP1/src/persistencia/PersistenciaMotorista.java
@@ -0,0 +1,20 @@
+package persistencia;
+
+import modelo.Motorista;
+
+public class PersistenciaMotorista {
+	
+	public void salvaMotorista(Motorista motorista) {
+		//TODO implementar o método de salvar
+	}
+	
+	public boolean pesquisaMotorista(Motorista motorista) {
+		//TODO implementar a pesquisa
+		return false;
+	}
+	
+	public boolean deletaMotorista(Motorista motorista) {
+		//TODO implementar a deleção do motorista
+		return false;
+	}
+}
